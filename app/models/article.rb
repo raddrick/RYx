3919febe730e3028dd5013293812029f025f2b2d
Rick Graham <rick@raddstudio.com>
2016-07-14 17:53:31 +0100

    add relationships to article and landing

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,4 @@
 class Article < ActiveRecord::Base
+  belongs_to :landing
+  belongs_to :author, class_name: :user
 end
--- app/models/landing.rb
+++ app/models/landing.rb
@@ -1,2 +1,3 @@
 class Landing < ActiveRecord::Base
+  has_many :articles
 end
