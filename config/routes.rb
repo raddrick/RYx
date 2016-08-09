bf217ba44731bcd40266be6ca55ad8c597ef3f2c
Rick Graham <rick@raddstudio.com>
2016-08-09 05:15:02 +0100

    add a team landing page

--- config/routes.rb
+++ config/routes.rb
@@ -5,12 +5,12 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-
   root 'landings#show', id: 1
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
   get '/about' => 'landings#show', id: 2
+  get '/team' => 'landings#show', id: 3
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,7 @@
 
 Landing.find_or_create_by name: 'Welcome to RYx', body: "RYx was created by Magellan"
 Landing.find_or_create_by name: 'About', body: "About RYxGlobal"
+Landing.find_or_create_by name: 'Team', body: "The RYxGlobal Team"
 
 #TODO we are seeding speakers because we do not want to go live with
 #  CMS features for Speakers/Talks but we do want the UI features
