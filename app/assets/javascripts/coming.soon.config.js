2a1c508f1ef409f9cd707bafcb163312e0219afd
Rick Graham <rick@raddstudio.com>
2016-09-11 22:28:44 +0300

    update the event date

--- app/assets/javascripts/coming.soon.config.js
+++ app/assets/javascripts/coming.soon.config.js
@@ -1,9 +1,9 @@
 (function($){
 	$(document).ready(function(){
 		// Set the time at which the countdown expires.
-		// var untilDate new Date(Year, Month - 1, Day, Hours -1, minutes)
+		// var untilDate new Date(Year, Month - 1, Day, Hours - 1, minutes)
 		//-----------------------------------------------
-		var untilDate = new Date(Date.UTC(2016, 9 - 1, 27, 18 - 1, 30));
+		var untilDate = new Date(Date.UTC(2016, 9 - 1, 26, 19 - 4, 30));
 
 		$(".countdown").countdown({
 			until: untilDate,
